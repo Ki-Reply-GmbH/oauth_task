62716eceddd027385a67e551528764d581c9beea
Pratik Saha <p.saha@reply.de>
2025-02-26 15:16:48 +0100

    fix: initialize the keys

--- main.go
+++ main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"oauth-basic/src/handlers"
+	"oauth-basic/src/keys"
 )
 
-// healthHandler responds with "Server is up" when /health is requested.
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Server is up")
 }
 
 func main() {
-	// Register the /health route with its handler
-	http.HandleFunc("/health", healthHandler)
 
-	// Start the server on port 8080
+	keys.InitializeKeys()
+	/* * because its a small project with small number of endpoint so i keep it in main
+	but usually i would use Gorilla Mux, Chi, or the built-in http.ServeMux and register the routes in a separate file.
+	*/
+	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/token", handlers.TokenHandler)
+
 	fmt.Println("Server is running on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Error starting server:", err)
