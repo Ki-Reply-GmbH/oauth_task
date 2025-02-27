a0fc37e1a49b3ecfe11569394b1037b1eaffce10
Pratik Saha <p.saha@reply.de>
2025-02-27 15:16:43 +0100

    update doc file to point to port 80

--- docs/docs.go
+++ docs/docs.go
@@ -175,7 +175,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "localhost:80",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "OAuth2 Server API",
